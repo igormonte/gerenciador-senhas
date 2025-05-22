82d4b2547c71b82aa094f4e415dc8e7eb856277d
Leandro <leandro_bt26@yahoo.com>
2025-05-21 21:41:16 -0300

    adicionado codigo em exibir variavel, para exibir as informações no terminal

---
+++ exibirvariavel.py
@@ -0,0 +1,8 @@
+codigo = 105
+nome = "jose Santana"
+salario = 1500.00
+ativo = True
+print ("codigo: %d " % codigo)
+print ("nome: %s " % nome)
+print ("salario: %.2f " % salario)  
+print ("ativo: %s " % ativo)
