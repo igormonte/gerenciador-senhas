4560f5cf6bb62cff0ae96720f79ed24bfabe8fff
Leandro <leandro_bt26@yahoo.com>
2025-05-23 02:04:07 -0300

    adicionado arquivo estrutura simples

---
+++ EstruturaSimples.py
@@ -0,0 +1,9 @@
+A = input("Informe um valor para a variável A: ")
+B = input("Informe um valor para a variável B: ")
+
+if (A>B):
+    troca=A;
+    A=B;
+    B=troca;
+print("O valor da variavel A é: ", A)
+print("O valor da variavel B é: ", B)
